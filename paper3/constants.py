3e7edd519ff9f717158a222e56bbeb1f9bd696c1
brogalla <brogalla@eoas.ubc.ca>
2024-03-01 07:24:07 -0800

    re-order supplementary figure numbers and add separate file for constants

---
+++ paper3/constants.py
@@ -0,0 +1,13 @@
+# Constants used throughout the notebooks
+
+# model dimensions:
+jmin, jmax = 159, 799
+imin, imax = 1139, 2179
+isize = imax-imin
+jsize = jmax-jmin
+
+# Boundary rimwidths:
+rimwidthN  = 10
+rimwidthS  = 10
+rimwidthW  = 10
+rimwidthE  = 20
